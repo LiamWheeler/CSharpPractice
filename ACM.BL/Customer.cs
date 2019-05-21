a483c04a3f0edbb033cc90022876664a938451ba
liam wheeler <liam_wheeler@hotmail.com>
2019-05-21 17:11:58 +0100

    Static Test

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +8,8 @@ namespace ACM.BL
 {
     public class Customer
     {
-        public int CustomerId { get; private set; }
-
+        public int CustomerID { get; private set; }
         public string EmailAddress { get; set; }
-
         public string FirstName { get; set; }
 
         public string FullName
@@ -27,7 +25,7 @@ namespace ACM.BL
                     }
                     fullName += FirstName;
                 }
-            return FullName;
+                return fullName;
             }
         }
 
@@ -36,45 +34,13 @@ namespace ACM.BL
         private string _lastName;
         public string LastName
         {
-            get
-            {
-                return _lastName;
-            }
-            set
-            {
-                _lastName = value;
-            }
+            get { return _lastName; }
+            set { _lastName = value; }
         }
 
-        /// <summary>
-        /// Retrieve one customer
-        /// </summary>
-        /// <param name="customerId"></param>
-        /// <returns></returns>
-        public Customer Retrieve(int customerId)
-        {
-            //Code that retrieves the defined customer
-            return new Customer();
-        }
-
-        /// <summary>
-        /// Saves the current customer
-        /// </summary>
-        /// <returns></returns>
-        public bool Save()
-        {
-            //Code that saves the defined customer
-            return true;
-        }
-
-        /// <summary>
-        /// Validates the customer data.
-        /// </summary>
-        /// <returns></returns>
         public bool Validate()
         {
             var isValid = true;
-
             if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
             if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
 
