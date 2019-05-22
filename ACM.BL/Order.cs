df9ddd06a34b27bc8bdccd3589270b92206061f2
liam wheeler <liam_wheeler@hotmail.com>
2019-05-22 10:17:50 +0100

    Order class added

---
+++ ACM.BL/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+
+        /// <summary>
+        /// Retrieves an order
+        /// </summary>
+        /// <returns></returns>
+        public Order Retrieve()
+        {
+            //Code to retrieve the order
+            return new Order();
+        }
+
+        public bool save()
+        {
+            //Code to save the order
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+
+    }
+}
