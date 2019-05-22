f2232a6ad01510d379bbede74040a89f475e0868
liam wheeler <liam_wheeler@hotmail.com>
2019-05-22 10:07:43 +0100

    Product class

---
+++ ACM.BL/Product.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            ProductId = productId;
+        }
+
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal? CurrentPrice { get; set; }
+        public int ProductId { get; private set; }
+
+
+        /// <summary>
+        /// Retrieves one product
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public Product Retrieve(int productId)
+        {
+            //Code that retrieves the product
+            return new Product();
+        }
+
+        /// <summary>
+        /// Saves the current product
+        /// </summary>
+        /// <returns></returns>
+        public bool save()
+        {
+            //Code that saves the defined product
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the product data
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+
+        }
+
+
+    }
+}
