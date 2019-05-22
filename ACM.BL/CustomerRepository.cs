8f2ea8ced687851db10f9ef5352ac2cbb1a3f28f
liam wheeler <liam_wheeler@hotmail.com>
2019-05-22 11:56:00 +0100

    Customer repository class and test class

---
+++ ACM.BL/CustomerRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class CustomerRepository
+    {
+
+        /// <summary>
+        /// Retrieve one customer
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Customer Retrieve(int customerId)
+        {
+            //create the instance for the customer class
+            //pass in the requested id
+            Customer customer = new Customer(customerId);
+
+            //Code that retrieves the defined customer
+
+            //Temporary hardcoded values to return
+            //a populated customer
+
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "Lwheeler@test.me";
+                customer.FirstName = "Liam";
+                customer.LastName = "Wheeler";
+            }
+            return customer;
+        }
+
+        /// <summary>
+        /// Saves the current customer
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        public bool Save(Customer customer)
+        {
+            //Code that saves the passed in customer
+            return true;
+        }
+        
+    }
+}
